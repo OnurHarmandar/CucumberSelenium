bbe855be5d5198a2ea15c3c92151951528efb7cf
OnurHarmandar <harmandaronur@gmail.com>
2022-02-17 16:06:05 +0300

    changing driver

---
+++ src/test/java/com/vytrack/utilities/DBUtils.java
@@ -0,0 +1,18 @@
+package com.vytrack.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DBUtils {
+    public static void createConnection() {
+    }
+
+    public static void destroy() {
+    }
+
+    public static Map<String, Object> getRowMap(String query) {
+
+        Map<String,Object> rowMap=new HashMap<>();
+        return rowMap;
+    }
+}
